6da74ab53f414fa3a144d8f38a8891093653d216
samuelbrhanealemayohu <contact@samuelbrhane.com>
2026-02-10 10:02:39 +0300

    Export providers for Completion, Hover, Diagnostic, and Code Action functionalities

---
+++ vscode-extension/src/providers/index.ts
@@ -0,0 +1,4 @@
+export { CompletionProvider } from "./completionProvider";
+export { HoverProvider } from "./hoverProvider";
+export { DiagnosticProvider } from "./diagnosticProvider";
+export { CodeActionProvider } from "./codeActionProvider";
